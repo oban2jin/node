1ba6c8eae8d14d720492cc9afa8f361d8f936f6d
oban2jin <oban2jin@gmail.com>
2015-03-27 01:57:28 +0900

    part1

---
+++ learnyounode/program2.js
@@ -0,0 +1,10 @@
+// console.log(process.argv);
+
+var sum = 0;
+
+for(var i =2; i< process.argv.length ; i++){
+    sum = sum + Number(process.argv[i]);
+    // console.log(Number(process.argv[i]));
+}
+
+console.log(sum);
